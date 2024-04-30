387da60ce074169efa25268d073e1c4627068bde
Piero Nicolli <piero.nicolli@redturtle.it>
2024-04-30 13:15:37 +0200

    feat: configure osmmap to be used as view widget for geolocation in default view

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import { GeoLocationWidget } from 'volto-venue/components';
+import { OSMMap } from 'volto-venue/components';
+export { GeoLocationWidget, OSMMap };
 
-export { OSMMap } from 'volto-venue/components';
-export { GeoLocationWidget };
-
-export default (config) => {
+export default function applyConfig(config) {
   config.widgets.id = {
     ...config.widgets.id,
     geolocation: GeoLocationWidget,
   };
 
+  config.widgets.views.id.geolocation = ({ value }) => (
+    <OSMMap markers={[{ ...value, title: '' }]} />
+  );
+
   return config;
-};
+}
